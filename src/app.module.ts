c1a4a8018417f4ab8bf2b433b17db79d40c3ea6f
webtensei <webtensei@gmail.com>
2023-11-02 18:16:16 +0700

    initial nest + prisma + swagger

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+// src/main.ts
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('vodokanal42.ru | API')
+    .setDescription('Описание API сервиса Водоканал Таштагольского района')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+
+bootstrap();
