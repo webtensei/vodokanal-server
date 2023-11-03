7e401e712b03ce6bd59095bb43a4335c0ba47f71
webtensei <webtensei@gmail.com>
2023-11-04 00:22:28 +0700

    contact module cascade added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@prisma/prisma.module';
 import { UserModule } from '@user/user.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), UserModule, PrismaModule, ContactModule],
   controllers: [],
   providers: [],
   exports: [],
