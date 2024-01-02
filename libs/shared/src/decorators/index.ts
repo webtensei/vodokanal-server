0d013b23fcb4baca1849c4fbd90ff9a8dab98b0e
webtensei <webtensei@gmail.com>
2024-01-02 13:29:15 +0700

    Role guard

--- libs/shared/src/decorators/index.ts
+++ libs/shared/src/decorators/index.ts
@@ -3,3 +3,4 @@ export * from './user-agent.decorator';
 export * from './public.decorator';
 export * from './ipdoor.decorator';
 export * from './current-user.decorator';
+export * from './roles.decorator';
--- src/auth/guards/index.ts
+++ src/auth/guards/index.ts
@@ -1,3 +1,4 @@
 import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@auth/guards/role.guard';
 
-export const GUARDS = [JwtAuthGuard];
+export const GUARDS = [JwtAuthGuard, RolesGuard];
